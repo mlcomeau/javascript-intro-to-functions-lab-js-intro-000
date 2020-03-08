0e2b1507b441994a61a5da09593ed7d001374494
Megan Comeau <mlcomeau99@gmail.com>
2020-03-08 22:28:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+  var uppercase = string.toUpperCase()
+  var lowercase = string.toLowerCase()
+  if (lowercase === string) {
+    return "I can't hear you!"
+  }
+  else if (uppercase === string) {
+    return "YES INDEED!"
+  } else (string === "I love you, Grandma.") {
+    return "I love you, too!"
+  }
+
+  }
+
+}
